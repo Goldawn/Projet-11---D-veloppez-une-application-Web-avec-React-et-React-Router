c55e2aaab175afdf74b0faefe64a60840a13ca1e
Goldawn <heunow@gmail.com>
2022-11-24 02:54:49 +0100

    header logo now redirects to homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import logo from "../../assets/icons/logo.svg"
 import Navbar from "../Navbar/Navbar"
 import './Header.css'
-// import {Link} from 'react-router-dom'
 
 export default function Header() {
     return (
         <header>
-            <img src={logo} className="logo" alt="logo" />
+            <Link to="/"><img src={logo} className="logo" alt="logo" /></Link>
             <Navbar />
         </header>
     )
